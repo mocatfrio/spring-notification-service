53c9e27124e2a0c6de769a2c0abe683950e66438
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2019-05-18 00:35:36 +0700

    Make ID Read Only in Documentation

--- notification-api/src/main/java/id/ac/its/notification_api/entity/Device.java
+++ notification-api/src/main/java/id/ac/its/notification_api/entity/Device.java
@@ -1,5 +1,6 @@
 package id.ac.its.notification_api.entity;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -13,6 +14,7 @@ import java.util.UUID;
 public class Device {
     @Id
     @GeneratedValue
+    @ApiModelProperty(readOnly = true)
     private UUID id;
     @Column(name = "token", nullable = false)
     @NotBlank
